c5c041ae5c02bb152932f164c10cddfeb8a3c49d
krutagya <krutagya.kaneria.cg@gmail.com>
2024-12-18 14:50:23 +0530

    1st commit

---
+++ que1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age = 9;
+    if(age>18){
+        cout << "eligible to vote";
+    }
+    else{
+        cout << "Not eligible to vote";
+    }
+    return 0;
+}
+
+int main() {
+    string password = "123456"
+    str 
+}
+
