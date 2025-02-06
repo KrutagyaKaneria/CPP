ea5aad231c088fef743b64dba5f13d21bc569d81
krutagya <krutagya.kaneria.cg@gmail.com>
2025-02-06 23:29:40 +0530

    oops

---
+++ oops/practise.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+
+// multilevel                          multilevel
+
+class one{
+public:
+   int marks;
+   string name;
+   string Grade; 
+};
+
+int main(){
+    one obj1;
+    obj1.marks = 10;
+    obj1.name ="krutagya";
+    obj1.Grade = "A"
+}
+
+
+
+class two{
+private:
+   int marks;
+   string name;
+
+   two(){
+      marks=50;
+      name="krutagya";
+   }
+};
+
+int main(){
+
+}
+
+
+
+
+
