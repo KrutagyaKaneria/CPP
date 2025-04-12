07dbe5edbb44b9f513454bc3235ab4455018bec6
krutagya <krutagya.kaneria.cg@gmail.com>
2025-04-12 09:13:02 +0530

    commit

--- filr.cpp
+++ filr.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
-#include <fstream>
-using namespace std;
+// #include <iostream>
+// #include <fstream>
+// using namespace std;
 
-int main() {
-    ifstream inFile("data.txt");
-    string line;
-    if (inFile.is_open()) {
-        while (getline(inFile, line)) {
-            cout << line << endl;
-        }
-        inFile.close();
-    } else {
-        cout << "Error opening file." << endl;
-    }
-    return 0;
-}
+// int main() {
+//     ifstream inFile("data.txt");
+//     string line;
+//     if (inFile.is_open()) {
+//         while (getline(inFile, line)) {
+//             cout << line << endl;
+//         }
+//         inFile.close();
+//     } else {
+//         cout << "Error opening file." << endl;
+//     }
+//     return 0;
+// }
 
 
 // #include <iostream>
@@ -33,3 +33,41 @@ int main() {
 //     }
 //     return 0;
 // }
+
+// #include <iostream>
+// #include <fstream>
+// using namespace std;
+
+// int main() {
+//     fstream file("data.txt", ios::in);
+//     if (file.is_open()) {
+//         file.seekg(10, ios::beg); // Move to 10th character
+//         char ch;
+//         file.get(ch);
+//         cout << "Character at position 10: " << ch << endl;
+//         file.close();
+//     } else {
+//         cout << "Error opening file." << endl;
+//     }
+//     return 0;
+// }
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    fstream file("data.txt", ios::in);
+    if (file.is_open()) {
+        file.seekp(10, ios::beg); // Move to 10th character
+        // char ch;
+        // file.get(ch);
+        // cout << "Character at position 10: " << ch << endl;
+        file << "muresh";
+        
+        file.close();
+    } else {
+        cout << "Error opening file." << endl;
+    }
+    return 0;
+}
