7da0eb931e975aff276825398d4d6ccab56e3f2b
krutagya <krutagya.kaneria.cg@gmail.com>
2025-05-09 12:09:16 +0530

    commit

--- loom_recording/Question2.cpp
+++ loom_recording/Question2.cpp
@@ -5,12 +5,10 @@ using namespace std;
 
 void printStringVector() {
     vector<string> vec = {"Apple", "Banana", "Cherry"};
-
     for (const string& str : vec) {
         cout << str << "\n";
     }
 }
-
 int main(){
     printStringVector();
     return 0;
